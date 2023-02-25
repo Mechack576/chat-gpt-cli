f00cad7d22df3baecc4fb5163085091e90e1b27a
Mechack576 <59377042+Mechack576@users.noreply.github.com>
2023-02-25 00:19:25 -0500

    Add files via upload

---
+++ go.mod
@@ -0,0 +1,11 @@
+module github.com/mechack/chat-gpt-cli
+
+go 1.16
+
+require (
+	github.com/PullRequestInc/go-gpt3 v1.1.11
+	github.com/joho/godotenv v1.3.0
+	github.com/spf13/cobra v1.6.1
+	github.com/stretchr/testify v1.8.1 // indirect
+	golang.org/x/net v0.4.0 // indirect
+)
---
+++ main.go
@@ -0,0 +1,68 @@
+package main
+
+import (
+	"bufio"
+	"context"
+	"fmt"
+	"os"
+
+	"github.com/PullRequestInc/go-gpt3"
+	"github.com/joho/godotenv" // package used to read the .env file
+	"github.com/spf13/cobra"
+)
+
+func GetResponse(client gpt3.Client, ctx context.Context, question string) {
+	err := client.CompletionStreamWithEngine(ctx, gpt3.TextDavinci003Engine, gpt3.CompletionRequest{
+		Prompt: []string{
+			question,
+		},
+		MaxTokens:   gpt3.IntPtr(3000),
+		Temperature: gpt3.Float32Ptr(0),
+	}, func(resp *gpt3.CompletionResponse) {
+		fmt.Print(resp.Choices[0].Text)
+	})
+	if err != nil {
+		fmt.Println(err)
+		os.Exit(13)
+	}
+	fmt.Printf("\n")
+}
+
+type NullWriter int
+
+func (NullWriter) Write([]byte) (int, error) { return 0, nil }
+
+func main() {
+	godotenv.Load()
+	apiKey := os.Getenv("API_KEY")
+	if apiKey == " " {
+		panic("Missing api key")
+	}
+
+	ctx := context.Background()
+	client := gpt3.NewClient(apiKey)
+	rootCmd := &cobra.Command{
+		Use:   "chatgpt",
+		Short: "chat with ChatGPT in console.",
+		Run: func(cmd *cobra.Command, arg []string) {
+			scanner := bufio.NewScanner(os.Stdin)
+			quit := false
+
+			for !quit {
+				fmt.Print("Say something ('quit' to end):")
+				if !scanner.Scan() {
+					break
+				}
+				question := scanner.Text()
+				switch question {
+				case "quit":
+					quit = true
+				default:
+					GetResponse(client, ctx, question)
+				}
+			}
+		},
+	}
+	rootCmd.Execute()
+
+}
